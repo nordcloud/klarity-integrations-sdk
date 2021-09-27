d966c2153c0597f3124a11a2f700b74be78da4ba
pochmurnyGrzech <grzegorz.kotlarz@nordcloud.com>
2021-09-27 14:35:40 +0200

    Change generator

--- python/integrations/git_push.sh
+++ python/integrations/git_push.sh
@@ -19,7 +19,7 @@ if [ "$git_user_id" = "" ]; then
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="klarity-integrations-sdk/python/integrations"
+    git_repo_id="klarity-integrations-sdk"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
